704d64c886f1c211e6843aedd8933f0fe56cc625
Akil Hamilton <akil.james83@gmail.com>
2021-03-30 00:52:30 -0400

    Add documentation

--- src/components/auth.js
+++ src/components/auth.js
@@ -4,22 +4,34 @@ import { GoogleLogin, GoogleLogout } from 'react-google-login';
 const clientId = '714538910544-jsqnfiagajp4k3ls4u2cfhod215qt9b5.apps.googleusercontent.com'
 
 const Auth = () => {
-  
+  const [showButton, toggleShow] = useState(true);
+
+  /**
+   * Handler for when a user sucessfully logs into google
+   * 
+   * @param response - response object from google when a person successfully logs in
+   */
   const success = (response) => {
     toggleShow(false);
     console.log(response);
   }
   
+  /**
+   * Handler for when a user unsucessfully logs into google
+   * 
+   * @param response - response object from google when a person unsuccessfully logs in
+   */
   const error = (response) => {
     console.error(response);
   }
   
+  /**
+   * Handler for when a user attempts to logout
+   */
   const logout = () => {
     toggleShow(true);
     console.log('logout');
   }
-
-  const [showButton, toggleShow] = useState(true);
 
   if (showButton) {
     return (
