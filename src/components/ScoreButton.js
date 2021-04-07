d2e0ae9fbb5abd177969fa13eef9cf44d3d9b02f
chiud8 <chiud8@mcmaster.ca>
2021-04-07 18:29:10 -0400

    changed to toggle score button

--- src/components/ScoreButton.js
+++ src/components/ScoreButton.js
@@ -6,37 +6,44 @@ class ScoreButton extends Component {
         this.state = {
             userID: 0,
             userName: 'User',
-            gameID: -1,
-            gameScore: 0,
-            text: ''
+            score1: 0,
+            score2: 0,
+            text: 'dummy',
+            showScore: false
         };
 
-        this.regenerateText();
-
         // This binding is necessary to make `this` work in the callback
+        this.regenerateText = this.regenerateText.bind(this);
         this.handleClick = this.handleClick.bind(this);
+
+        this.regenerateText();
     }
 
     regenerateText() {
         this.setState(state => ({
-            text: this.state.userName + ' (#' + this.state.userID + ') Score on Game '
-                + this.state.gameID + ': ' + this.state.gameScore
+            text: state.userName + ' (#' + state.userID + '): Score on Game 1 is '
+                + state.score1 + ', Score on Game 2 is ' + state.score2
         }));
     }
 
     handleClick() {
         this.regenerateText();
-        /*this.setState(state => ({
-            isToggleOn: !state.isToggleOn
-        }));*/
+        this.setState(state => ({
+            showScore: !state.showScore
+        }));
     }
 
     render() {
         const {text} = this.state;
+
         return (
             <div>
-                <button onClick={this.handleClick}>Update Score</button>
-                <p>{text}</p>
+                <button onClick={this.handleClick}>Toggle Score Display</button>
+                { this.state.showScore &&
+                    <p>
+                        {text}
+                    </p>
+                }
             </div>
         );
     }
