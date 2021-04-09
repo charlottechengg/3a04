339d1c140a154149e8e5997f9d6266a98dd4a276
Maninder Sran <maninder.s89@outlook.com>
2021-04-09 16:11:03 -0400

    FAQ initial push

---
+++ src/components/FAQ.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react'
+import Accordion from 'react-bootstrap/Accordion'
+import Card from 'react-bootstrap/Card'
+
+export class FAQ extends Component {
+    render() {
+        return (
+            <>
+                <div>
+                    <Accordion>
+                        <Card>
+                            <Accordion.Toggle as={Card.Header} eventKey="0">
+                                Question 1
+                            </Accordion.Toggle>
+
+                            <Accordion.Collapse eventKey="0">
+                                <Card.Body>Answer 1</Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+
+                        <Card>
+                            <Accordion.Toggle as={Card.Header} eventKey="1">
+                                TAB 2
+                            </Accordion.Toggle>
+
+                            <Accordion.Collapse eventKey="1">
+                                <Card.Body>This is second tab body</Card.Body>
+                            </Accordion.Collapse>
+                        </Card>
+                    </Accordion>
+                </div>
+            </>
+        );
+    }
+}
+
+export default FAQ;
