4ef9dbad4e5c4de2021c048f3b033450264ff88c
Manyi Cheng <chengm33@mcmaster.ca>
2021-04-09 21:27:58 -0400

    mathgame

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import MinigameMenu from './components/MinigameMenu.js'
 import Chalkboard from './components/Chalkboard.js';
+import Homepage from './components/'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+
       </header>
     </div>
   );
