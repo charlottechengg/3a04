7ef2d9b808b8b26be2e3aefdbe1cb327d00d8d87
Manyi Cheng <chengm33@mcmaster.ca>
2021-04-11 02:04:20 -0400

    added minigame 3 and updated minigame menu

--- src/components/MinigameMenu.js
+++ src/components/MinigameMenu.js
@@ -3,6 +3,7 @@ import "../MinigameMenu.css";
 import chalkboardImg from "./Assets/menu-minigame1.png";
 import matchImg from "./Assets/menu-minigame2.png";
 import lampImg from "./Assets/lamp.png";
+import wuhooImg from "./Assets/menu-minigame3.png";
 import { useHistory } from "react-router-dom";
 const MinigameMenu = () => {
   const history = useHistory();
@@ -22,6 +23,7 @@ const MinigameMenu = () => {
             <div className="card__content">
               <div className="card__title">Chalkboard Challenge</div>
               <p className="card__text">
+                Author: Manyi Cheng. <br/>
                 Challenge your calculation and estimation with our best math
                 game. Chalkboard Challenge improves the quantitative reasoning
                 and problem-solving skills.{" "}
@@ -45,6 +47,23 @@ const MinigameMenu = () => {
                 Challenge yourself with the most popular memory game. Quickly
                 memorizing and comparing the pokemons challenges your working
                 memory.
+              </p>
+              <button
+                className="card__btn"
+                onClick={() => handleMath("Memory")}
+              >
+                Start
+              </button>
+            </div>
+          </span>
+        </span>
+        <span className="cards__item">
+          <span className="card">
+            <img className="card__image" src={wuhooImg} alt="chalkboard" />
+            <div className="card__content">
+              <div className="card__title">Wuhoo!</div>
+              <p className="card__text">
+                Challenge your knowledge with the most popular quiz game. Pick the correct option challenges your understanding of computer science.
               </p>
               <button
                 className="card__btn"
